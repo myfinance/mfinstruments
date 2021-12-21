736248c0ea59cf59d5178d96be01b1954e3ed66b
holgerfischer <holger@hfischer.org>
2021-12-21 11:54:42 +0000

    init
    Signed-off-by: holgerfischer <holger@hfischer.org>

---
+++ instrumentservice/src/test/java/de/hf/myfinance/instrumentservice/InstrumentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hf.myfinance.instrumentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
